d9fac6a6e4f81d93a241dc6850418887908f7718
subaru-hello <octosubaru0926@gmail.com>
2023-01-02 14:39:56 +0900

    add Database context

---
+++ Startup.cs
@@ -0,0 +1,49 @@
+using RazorPagesMovie.Data;
+using Microsoft.EntityFrameworkCore;
+namespace RazorPagesMovie
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        #region snippet_ConfigureServices
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+
+            services.AddDbContext<RazorPagesMovieContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("RazorPagesMovieContext")));
+        }
+        #endregion
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
